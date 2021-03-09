23d84d1ea9cf596cba9e22285e1f8bda234be989
PabloEscobarFinance <pabloescobarfinance@protonmail.com>
2021-03-09 04:53:53 +0100

    update

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2749,7 +2749,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#222222", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#000000", textDisabled: "#BDC2C4", textSubtle: "#1a1a1a", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#ffffff", background: "#1b1d1e", backgroundDisabled: "#242728", contrast: "#ffffff", invertedContrast: "#191326", input: "#5a3f3f", primaryDark: "#0098A1", tertiary: "#104404", text: "#e8e6e3", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#634b4b", card: "#222222", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#ffffff", background: "#1b1d1e", backgroundDisabled: "#242728", contrast: "#ffffff", invertedContrast: "#106727", input: "#60865a", primaryDark: "#0098A1", tertiary: "#104404", text: "#e8e6e3", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#60865a", card: "#222222", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
